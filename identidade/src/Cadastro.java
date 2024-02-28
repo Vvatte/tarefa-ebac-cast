b4a341bca5c538e5822c8ecd2461156c58e0fc66
Vvatte <vvatte@yahoo.com>
2024-02-28 17:17:53 -0300

    descomplicando

--- identidade/src/Cadastro.java
+++ identidade/src/Cadastro.java
@@ -3,15 +3,12 @@ import java.util.Scanner;
 public class Cadastro {
 
     public static void main(String[] args) {
-        System.out.println("Digite seu Cpf");
         Scanner cpf = new Scanner(System.in);
+        System.out.println("Digite seu Cpf");
+        cpf.nextInt();
 
-        System.out.println("Digite seu Rg");
-        Scanner rg = new Scanner(System.in);
+        System.out.println("Seu Cpf:" + cpf);
 
-        System.out.println("Digite sua idade");
-        Scanner idade = new Scanner(System.in);
-
-        System.out.println("Seu Cpf:" + cpf + ", seu Rg:" + rg + ",sua idade:" + idade);
+        cpf.close();
     }
 }
