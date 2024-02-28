aeaaa45ca222bbbb2511d44d63f8af132609a419
Vvatte <vvatte@yahoo.com>
2024-02-28 17:21:52 -0300

    tentando arrumar

--- identidade/src/Cadastro.java
+++ identidade/src/Cadastro.java
@@ -4,6 +4,7 @@ public class Cadastro {
 
     public static void main(String[] args) {
         Scanner cpf = new Scanner(System.in);
+        Integer cpf;
         System.out.println("Digite seu Cpf");
         cpf.nextInt();
 
